a2c3d22222c857f7d98091ff91b300589637e9c6
haliltuzcu <007canberk@gmail.com>
2023-02-02 13:00:22 +0300

    son hali

--- todo.js
+++ todo.js
@@ -1,49 +1,129 @@
-const input = document.getElementById("input");
-const ul = document.getElementById("ul");
-const buton = document.getElementById("btn");
-let todos = [];
+//Elements
+const todoInput = document.getElementById("todo-input");
+const addBtn = document.querySelector("#todo-button");
+const todoUl = document.querySelector("#todo-ul");
 
-buton.addEventListener("click", () => {
-  const inputText = input.value.trim();
-  if (inputText == null || input.value == "") {
-    alert("deger giriniz");
-  } else {
-      const clik = document.createElement("input");
-      clik.className = "clik";
-      clik.type = "checkbox";
-      clik.name = "tmm";
-    const li = document.createElement("li");
-    li.className = "li";
+//global array for todo obj
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
-    li.innerHTML = inputText;
-    li.appendChild(clik);
-    ul.appendChild(li);
-    addtodolist(inputText);
-  }
+//load event vs. DomContentLoaded
+window.addEventListener("load", () => {
+    getTodoListFromLocalStorage();
+});
 
-  function addtodolist(newTodo) {
-    checkTodolist();
-    todos.push(newTodo);
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }
+const getTodoListFromLocalStorage = () => {
+    //get TodoList from localStorage and load to UI
+    todoList.forEach((todo)=>{
+        createTodo(todo);
+    });
+}
 
-  function checkTodolist() {
-    if (localStorage.getItem("todos") === null) {
-      todos = [];
-    } else {
-      todos = JSON.parse(localStorage.getItem("todos"));
+//form => submit event vs. button => click event
+// form.addEventListener("submit", ()=>{})
+addBtn.addEventListener("click", (e) => {
+    //prevent form submit
+    e.preventDefault();
+    //user input control
+    if(todoInput.value.trim() === ""){
+        alert("Please, enter new todo text!");
+        return;
     }
-  }
-  input.value = "";
-  input.focus();
-});
+    // else{
+    //     alert("continue");
+    // }
+    //continue func.
+    const newTodo = {
+        id : new Date().getTime(), //unique id with ms of now
+        completed : false, //status
+        text : todoInput.value //userInput
+    }
 
-input.addEventListener("keydown", (event) => {
-  if (event.code === "Enter") {
-    buton.click();
-  }
-});
+    // insertTodoToDB(newTodo);
+    createTodo(newTodo);
+    
+    //UPDATE TODO array
+    todoList.push(newTodo);
+    //localStorage todoList Update
+    //localStorage vs. SessionStorage vs. Cookies
+    //!!!!!!!!stringify!!!!!!!!!
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    //event.target vs. event.currentTarget
+    e.target.closest("form").reset();
+})
 
-delimg.addEventListener("click", () => {
-  ul.child(li);
+const createTodo = (newTodo) =>{
+    //todo item creation
+    //alert("item was added");
+    //obj. dest. (ES6 => JS'e kazandırılan yapılar??)
+    const { id, completed, text } = newTodo;
+
+    //create li
+    const li = document.createElement("li");
+    li.setAttribute("id", id);
+
+    //add class with completed(status)
+    completed ? li.classList.add("checked") : "";
+
+    //create check icon
+    const icon = document.createElement("i");
+    icon.setAttribute("class", "fas fa-check");
+    //append vs. appendChild
+    li.append(icon);
+
+    //create item text
+    const p = document.createElement("p");
+    p.innerText = text;
+    li.appendChild(p);
+
+    //create remove icon
+    const removeIcon = document.createElement("i");
+    removeIcon.setAttribute("class", "fas fa-trash");
+    li.append(removeIcon);
+
+    // console.log(li);
+
+    //append li to ul
+    //prepend vs. append
+    // todoUl.append(li);
+    todoUl.prepend(li);
+
+}
+
+//Capturing vs. Bubbling
+//static closest parent element => child
+todoUl.addEventListener("click", (e)=>{
+    const idAttr = e.target.closest("li").getAttribute("id");
+    if(e.target.classList.contains("fa-check")){
+        // alert("check clicked");
+        //update UI
+        e.target.parentElement.classList.toggle("checked");
+        //update array
+        // todoList.map((todo)=>{
+        //     if(todo.id == idAttr){
+        //         todo.completed = !todo.completed;
+        //     }
+        // });
+        todoList.forEach((todo)=>{
+            if(todo.id == idAttr){
+                todo.completed = !todo.completed;
+            }
+        });
+        //add updated array to localStorage 
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+    else if(e.target.classList.contains("fa-trash")){
+        // alert("remove clicked");
+        //remove from UI
+        e.target.parentElement.remove();
+        //remove from Array
+        //id si ile silinmeyenleri filtrele array i update et ==> silineni array den remove
+        todoList = todoList.filter((todo)=> todo.id != idAttr);
+        //add updated array to localStorage 
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+
+    }
+    else{
+        alert("other element clicked");
+    }
+    console.log(todoList);
 });
